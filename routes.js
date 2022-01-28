ae3eb2fea58f1b5cd778d3718c5d4d3cd9c211e0
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-28 13:26:53 +0100

    config changes - DEV OK

--- routes.js
+++ routes.js
@@ -12,43 +12,52 @@ const {
 
 const { handleNoUserData } = require('./middleware')
 
-
-router.get('/api/user/:id', (req, res) => {
+/**
+ * @route   GET api/user/:id
+ * @desc    Get user basic info
+ * @access  Public
+ */
+router.get('/:id', (req, res) => {
     const userId = idx(req, _ => _.params.id)
     const userData = getUserById(Number(userId))
 
     return handleNoUserData(res, userData)
 })
 
-
-router.get('/api/user/:id/activity', (req, res) => {
+/**
+ * @route   GET api/user/:id/activity
+ * @desc    Get user activity info
+ * @access  Public
+ */
+router.get('/:id/activity', (req, res) => {
     const userId = idx(req, _ => _.params.id)
     const userData = getUserActivityById(Number(userId))
 
     return handleNoUserData(res, userData)
 })
 
-
-router.get('/api/user/:id/average-sessions', (req, res) => {
+/**
+ * @route   GET api/user/:id/average-sessions
+ * @desc    Get user average-sessions info
+ * @access  Public
+ */
+router.get('/:id/average-sessions', (req, res) => {
     const userId = idx(req, _ => _.params.id)
     const userData = getUserAverageSession(Number(userId))
 
     return handleNoUserData(res, userData)
 })
 
-
-router.get('/api/user/:id/performance', (req, res) => {
+/**
+ * @route   GET api/user/:id/performance
+ * @desc    Get user performance info
+ * @access  Public
+ */
+router.get('/:id/performance', (req, res) => {
     const userId = idx(req, _ => _.params.id)
     const userData = getUserPerformance(Number(userId))
 
     return handleNoUserData(res, userData)
 })
-
-// Resolve any address with built index.html (where spa root lives)
-// DEV:
-// router.get("/*", (_, res) => { res.sendFile(path.join(__dirname, "client/public/index.html")) })
-
-// PROD:
-router.get('/*', (_, res) => { res.sendFile(path.join(__dirname, 'client/build', 'index.html'))})
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,19 +3,26 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const app = express()
 const cors = require('cors')
-const router = require('./routes')
-const favicon = require('serve-favicon')
 const path = require('path')
+const favicon = require('serve-favicon')
 
 app.use(express.json())
 app.use(cors())
-app.use(router)
 
-// Handle custom routes
-app.use('/api/', require('./routes'))
 
-// Serve the static files from the React app
+const userRoutes =require('./routes');
+// Use Routes
+app.use('/api/user', userRoutes)
+
 //app.use(express.static('client/public/')) // DEV
-app.use(express.static(path.join(__dirname, '/client/build/'))); // PROD
+
+// Serve the static if in production
+if ( process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build/'))
+    // any other request than '/api/' => load 'index.html'
+    app.get('*', (_, res) => { res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))})
+
+}
 
 app.listen(PORT, () => { console.log(`Server listening on ${PORT}`)})
